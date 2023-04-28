83622453e46881146c3abc83669f2428b2301ac0
terry-guel <taerimyoon@korea.ac.kr>
2023-04-28 07:55:32 +0000

    shell

---
+++ make.sh
@@ -0,0 +1,14 @@
+while getopts "i:d:n:" opt; do
+  case ${opt} in
+    i ) IMAGE=${OPTARG};;
+    n ) NAME=${OPTARG};;
+    \? ) echo "Invalid option: -$OPTARG" 1>&2; exit 1;;
+    : ) echo "Option -$OPTARG requires an argument." 1>&2; exit 1;;
+  esac
+done
+
+echo "IMAGE: $IMAGE"
+echo "NAME: $NAME"
+echo "UID: $UID"
+
+docker build -t ${IMAGE} --build-arg UNAME=${NAME} --build-arg UID=${UID} --build-arg GID=${UID} .
